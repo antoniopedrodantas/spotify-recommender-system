8056b19534728892756564e2b3a07f5dbf4f2dd1
Pedro Dantas <pedro.dantascv@gmail.com>
2022-03-30 15:54:25 +0100

    added .env file and environment variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+// require('dotenv').config();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{process.env.REACT_APP_CLIENT_ID}</p>
+      <p>{process.env.REACT_APP_CLIENT_SECRET}</p>
     </div>
   );
 }
